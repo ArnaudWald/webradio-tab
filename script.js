f15c1df11e9ee2263e2c04f54f0caac3b718bd36
Wald <arnaud.wald@bearingpoint.com>
2018-03-04 11:01:18 +0100

    Adda third webradio

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function() {
 	// audio
 	var audio = document.getElementById("audio");
 	var audio2 = document.getElementById("audio2");
+	var audio3 = document.getElementById("audio3");
 
 	// Buttons
 	var container = document.getElementById("audio-container");
@@ -10,6 +11,9 @@ window.onload = function() {
 	
 	var container2 = document.getElementById("audio2-container");
 	var playButton2 = document.getElementById("audio2-controls");
+	
+	var container3 = document.getElementById("audio3-container");
+	var playButton3 = document.getElementById("audio3-controls");
 
 	// Event listener for the play/pause button
 	container.addEventListener("click", function() {
@@ -17,13 +21,17 @@ window.onload = function() {
 			// Play the audio
 			audio.play();
 			audio2.pause();
+			audio3.pause();
 
 			// Update the button text to 'Pause'
 			playButton.innerHTML = "<i class='fas fa-pause fa-7x'></i>";
 			playButton2.innerHTML = "<i class='fas fa-play fa-7x'></i>";
+			playButton3.innerHTML = "<i class='fas fa-play fa-7x'></i>";
 		} else {
 			// Pause the audio
 			audio.pause();
+			audio2.pause();
+			audio3.pause();
 
 			// Update the button text to 'Play'
 			playButton.innerHTML = "<i class='fas fa-play fa-7x'></i>";
@@ -35,17 +43,42 @@ window.onload = function() {
 			// Play the audio
 			audio2.play();
 			audio.pause();
+			audio3.pause();
 
 			// Update the button text to 'Pause'
 			playButton2.innerHTML = "<i class='fas fa-pause fa-7x'></i>";
 			playButton.innerHTML = "<i class='fas fa-play fa-7x'></i>";
+			playButton3.innerHTML = "<i class='fas fa-play fa-7x'></i>";
 		} else {
 			// Pause the audio
 			audio2.pause();
 			audio.pause();
+			audio3.pause();
 
 			// Update the button text to 'Play'
 			playButton2.innerHTML = "<i class='fas fa-play fa-7x'></i>";
+		}
+	});
+	
+	container3.addEventListener("click", function() {
+		if (audio3.paused == true) {
+			// Play the audio
+			audio3.play();
+			audio.pause();
+			audio2.pause();
+
+			// Update the button text to 'Pause'
+			playButton3.innerHTML = "<i class='fas fa-pause fa-7x'></i>";
+			playButton.innerHTML = "<i class='fas fa-play fa-7x'></i>";
+			playButton2.innerHTML = "<i class='fas fa-play fa-7x'></i>";
+		} else {
+			// Pause the audio
+			audio3.pause();
+			audio.pause();
+			audio2.pause();
+
+			// Update the button text to 'Play'
+			playButton3.innerHTML = "<i class='fas fa-play fa-7x'></i>";
 		}
 	});
 
