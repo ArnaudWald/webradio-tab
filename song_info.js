5e35fb2998f6daf5d1fe5ea0337ae8df86bdeebc
DuanraDlaw <arnaudwald@gmail.com>
2018-03-08 17:02:41 +0100

    Show Song info in footer

--- song_info.js
+++ song_info.js
@@ -1,6 +1,22 @@
 window.onload = function() {
+	// radio-title
+	var radio_title = document.getElementById("radio-title");
 
-	audio_controls()
+	// audio
+	var audio = document.getElementById("audio-general");
+	var audio_source = document.getElementById("audio-source");
+
+	// Buttons
+	var container = document.getElementById("audio-container");
+	var playButton = document.getElementById("audio-controls");
+
+	var container2 = document.getElementById("audio2-container");
+	var playButton2 = document.getElementById("audio2-controls");
+
+	var container3 = document.getElementById("audio3-container");
+	var playButton3 = document.getElementById("audio3-controls");
+
+	var playingStation = "";
 
 	function toTitleCase(str)
 	{
@@ -9,12 +25,14 @@ window.onload = function() {
 
 	function showSongInfo(song) {
 		var songName = document.getElementById("title");
+		var artiste = document.getElementById("artiste");
 		var album = document.getElementById("album");
 		var year = document.getElementById("year");
 		var label = document.getElementById("label");
 		var cover = document.getElementById("cover");
 
 		songName.innerHTML = toTitleCase(song["title"]);
+		artiste.innerHTML = toTitleCase(song["authors"]);
 		album.innerHTML = toTitleCase(song["titreAlbum"]);
 		year.innerHTML = song["anneeEditionMusique"];
 		label.innerHTML = toTitleCase(song["label"]);
@@ -23,35 +41,112 @@ window.onload = function() {
 
 	}
 
-	url = "https://www.fip.fr/livemeta/66";
+	function pauseAudio (buttonID) {
+		// Pause the audio
+		audio.pause();
+		playingStation = "";
 
-	var stationId = document.getElementById("stationId");
-	var song_info;
+		radio_title.innerHTML = "en pause";
 
-	fetch(url, {method: 'get'})
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+		// Update the button text to 'Play'
+		buttonID.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+	}
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-				song_info = data;
+	function playAudio (stationName) {
+		audio_source.src = stations[stationName]["stream"];
 
-				var songPosition = data["levels"][0]['position'];
-				var songStep = data["levels"][0]['items'][songPosition];
-				var song = data["steps"][songStep];
+		audio.load();
+		audio.play();
 
-				showSongInfo(song);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+		getSongInfo(stationName);
+
+		radio_title.innerHTML = stations[stationName]["name"];
+	}
+
+	audio_controls = function() {
 
 
+
+		// Event listener for the play/pause button
+		container.addEventListener("click", function() {
+			if (audio.paused == true || playingStation != "fip-paris") {
+				// Play the audio
+				playingStation = "fip-paris";
+				playAudio(playingStation);
+
+				// Update the button text to 'Pause'
+				playButton.innerHTML = "<i class='fas fa-pause fa-6x'></i>";
+				playButton2.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+				playButton3.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+			} else {
+				pauseAudio(playButton);
+			}
+		});
+
+		container2.addEventListener("click", function() {
+			if (audio.paused == true || playingStation != "theme-electro") {
+				// Play the audio
+				playingStation = "theme-electro";
+				playAudio(playingStation);
+
+				// Update the button text to 'Pause'
+				playButton2.innerHTML = "<i class='fas fa-pause fa-6x'></i>";
+				playButton.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+				playButton3.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+			} else {
+				pauseAudio(playButton2);
+			}
+		});
+
+		container3.addEventListener("click", function() {
+			if (audio.paused == true || playingStation != "theme-groove") {
+				// Play the audio
+				playingStation = "theme-groove";
+				playAudio(playingStation);
+
+				// Update the button text to 'Pause'
+				playButton3.innerHTML = "<i class='fas fa-pause fa-6x'></i>";
+				playButton.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+				playButton2.innerHTML = "<i class='fas fa-play fa-6x'></i>";
+			} else {
+				pauseAudio(playButton3);
+			}
+		});
+	}
+
+
+	function getSongInfo(playingStation) {
+		url = "https://www.fip.fr/livemeta/" + stations[playingStation]["channelId"];
+
+		var stationId = document.getElementById("stationId");
+		var song_info;
+
+		fetch(url, {method: 'get'})
+	  .then(
+	    function(response) {
+	      if (response.status !== 200) {
+	        console.log('Looks like there was a problem. Status Code: ' +
+	          response.status);
+	        return;
+	      }
+
+	      // Examine the text in the response
+	      response.json().then(function(data) {
+					song_info = data;
+
+					var songPosition = data["levels"][0]['position'];
+					var songStep = data["levels"][0]['items'][songPosition];
+					var song = data["steps"][songStep];
+
+					showSongInfo(song);
+	      });
+	    }
+	  )
+	  .catch(function(err) {
+	    console.log('Fetch Error :-S', err);
+	  });
+
+	}
+
+	audio_controls();
 }
---
+++ stations.js
@@ -0,0 +1,17 @@
+var stations = {
+  "fip-paris": {
+    "name": "FIP Paris",
+    "channelId": 7,
+    "stream": "https://direct.fipradio.fr/live/fip-midfi.mp3"
+  },
+  "theme-electro": {
+    "name": "FIP Autour de l'électro",
+    "channelId": 74,
+    "stream": "https://direct.fipradio.fr/live/fip-webradio8.mp3"
+  },
+  "theme-groove": {
+    "name": "FIP Autour du groove",
+    "channelId": 66,
+    "stream": "https://direct.fipradio.fr/live/fip-webradio3.mp3"
+  }
+}
